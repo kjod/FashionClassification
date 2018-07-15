502dcca82dce2f8f3b59ab81652910078e3a592b
kjod <kieranjamesodriscoll@gmail.com>
2018-07-15 16:18:37 +0200

    Added extra layer.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-#Adapted from
+# Adapted from
 # https://medium.com/ml2vec/intro-to-pytorch-with-image-classification-on-a-fashion-clothes-dataset-e589682df0c5
 import argparse
+
 import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
@@ -8,19 +9,40 @@ from torch.autograd import Variable
 from torch.utils.data import Dataset
 from torchvision import datasets, transforms
 
-#constants
 use_cuda = torch.cuda.is_available()
 kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
 device = torch.device("cuda" if use_cuda else "cpu")
+labels_map = {0: 'T-Shirt', 1: 'Trouser', 2: 'Pullover', 3: 'Dress', 4: 'Coat', 5: 'Sandal', 6: 'Shirt',
+              7: 'Sneaker', 8: 'Bag', 9: 'Ankle Boot'}
 
 parser = argparse.ArgumentParser()
 parser.parse_args()
-parser.add_argument("-batch_size", help="Batch size used", default=300)
-parser.add_argument("-num-epochs", help="Number of epochs used", default=5)
+parser.add_argument("-batch_size", help="Batch size used", default=50)
+parser.add_argument("-num-epochs", help="Number of epochs used", default=1)
 parser.add_argument("-learning_rate", help="CNN learning rate", default=0.001)
 args = parser.parse_args()
-
 print(args)
+
+def plot_kernels(tensor, num_cols=6):
+    num_kernels = tensor.shape[0]
+    num_rows = 1 + num_kernels // num_cols
+    fig = plt.figure(figsize=(num_cols, num_rows))
+    for i in range(num_kernels):
+        ax1 = fig.add_subplot(num_rows, num_cols, i + 1)
+        ax1.imshow(tensor[i][0, :, :], cmap='gray')
+        ax1.axis('off')
+        ax1.set_xticklabels([])
+        ax1.set_yticklabels([])
+    plt.show()
+
+def plot_losses(losses):
+    fig = plt.figure()
+    plt.xlabel('Batches #')
+    plt.ylabel('Loss')
+    plt.plot(losses)
+    #Add plot of losses per category
+    plt.show()
+
 
 class CNN(nn.Module):
     def __init__(self):
@@ -35,23 +57,28 @@ class CNN(nn.Module):
             nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(2))
-        self.fc = nn.Linear(7 * 7 * 32, 10)
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=5, padding=2),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2))
+        self.fc = nn.Linear(3 * 3 * 64, 10)
 
     def forward(self, x):
         out = self.layer1(x)
         out = self.layer2(out)
+        out = self.layer3(out)
         out = out.view(out.size(0), -1)
         out = self.fc(out)
         return out
 
 
-training_dataset = datasets.FashionMNIST('data', train=True, download=True,
-                                         transform=transforms.Compose([
-                                             transforms.ToTensor(),
-                                             transforms.Normalize((0.1307,), (0.3081,))
-                                         ]))
-train_loader = torch.utils.data.DataLoader(training_dataset
-                                           , batch_size=args.batch_size, shuffle=True, **kwargs)
+train_loader = torch.utils.data.DataLoader(
+    datasets.FashionMNIST('data', train=True, download=True,
+                          transform=transforms.Compose([
+                              transforms.ToTensor(),
+                              transforms.Normalize((0.1307,), (0.3081,))
+                          ])), batch_size=args.batch_size, shuffle=True, **kwargs)
 
 test_loader = torch.utils.data.DataLoader(
     datasets.FashionMNIST('data', train=False, download=True,
@@ -60,12 +87,30 @@ test_loader = torch.utils.data.DataLoader(
                               transforms.Normalize((0.1307,), (0.3081,))
                           ])), batch_size=args.batch_size, shuffle=True, **kwargs)
 
-# instance of the Conv Net
-cnn = CNN();
-criterion = nn.CrossEntropyLoss();
-optimizer = torch.optim.Adam(cnn.parameters(), lr=args.learning_rate);
+#Show some sample images
+fig = plt.figure()
 
-losses = [];
+for i in range(len(t)):
+    sample = face_dataset[i]
+
+    print(i, sample['image'].shape, sample['landmarks'].shape)
+
+    ax = plt.subplot(1, 4, i + 1)
+    plt.tight_layout()
+    ax.set_title('Sample #{}'.format(i))
+    ax.axis('off')
+    show_landmarks(**sample)
+
+    if i == 3:
+        plt.show()
+        break
+
+cnn = CNN()
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(cnn.parameters(), lr=args.learning_rate)
+N = int(train_loader.__len__())
+losses = []
+
 for epoch in range(args.num_epochs):
     for i, (images, labels) in enumerate(train_loader):
         images = Variable(images.float())
@@ -78,56 +123,47 @@ for epoch in range(args.num_epochs):
         loss.backward()
         optimizer.step()
 
-        losses.append(loss.data.item());
+        losses.append(loss.data.item())
 
         if (i + 1) % 100 == 0:
-            print('Epoch : %d/%d, Batch : %d/%d,  Loss: %.4f'
-                  % (epoch + 1, args.num_epochs, i + 1, len(training_dataset) // args.batch_size, loss.data.item()))
+            print('Epoch : {epoch_no}/{epoch_total}, Batch : {batch_no}/{N},  Loss: {loss:.{digits}f}'.format(
+                digits=2,
+                epoch_no=epoch + 1,
+                epoch_total=args.num_epochs,
+                batch_no=i + 1,
+                N=N,
+                loss=loss.data.item())
+            )
 
 cnn.eval()
 correct = 0
 total = 0
+
 for images, labels in test_loader:
     images = Variable(images.float())
     outputs = cnn(images)
     _, predicted = torch.max(outputs.data, 1)
     total += labels.size(0)
+    print(labels.shape)
+    print(labels)
     correct += (predicted == labels).sum()
-print('Test Accuracy: %.4f %%' % (100 * correct / total))
+print('Test Accuracy: {0:.4f}'.format(100 * correct / total))
 
-fig = plt.figure(figsize=(8,8));
+filters = cnn.modules()
+model_layers = [i for i in cnn.children()]
 
-# losses_in_epochs = losses[0::600]
-# plt.xlabel('Epoch #');
-# plt.ylabel('Loss');
-# plt.plot(losses_in_epochs);
-# plt.show();
+plot_losses(losses)
 
-
-def plot_kernels(tensor, num_cols=6):
-    num_kernels = tensor.shape[0]
-    num_rows = 1 + num_kernels // num_cols
-    fig = plt.figure(figsize=(num_cols, num_rows))
-    for i in range(num_kernels):
-        ax1 = fig.add_subplot(num_rows, num_cols, i + 1)
-        ax1.imshow(tensor[i][0, :, :], cmap='gray')
-        ax1.axis('off')
-        ax1.set_xticklabels([])
-        ax1.set_yticklabels([])
-
-
-plt.subplots_adjust(wspace=0.1, hspace=0.1)
-plt.show()
-filters = cnn.modules();
-model_layers = [i for i in cnn.children()];
-first_layer = model_layers[0];
-second_layer = model_layers[1];
+first_layer = model_layers[0]
 first_kernels = first_layer[0].weight.data.numpy()
 plot_kernels(first_kernels, 8)
+
+second_layer = model_layers[1]
 second_kernels = second_layer[0].weight.data.numpy()
 plot_kernels(second_kernels, 8)
 
-labels_map = {0: 'T-Shirt', 1: 'Trouser', 2: 'Pullover', 3: 'Dress', 4: 'Coat', 5: 'Sandal', 6: 'Shirt',
-              7: 'Sneaker', 8: 'Bag', 9: 'Ankle Boot'};
+third_layer = model_layers[2]
+third_kernels = third_layer[0].weight.data.numpy()
+plot_kernels(third_kernels, 8)
 
-#Line chart of losses based on certain classifications
+# Line chart of losses based on certain classifications
